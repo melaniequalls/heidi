7a8cdf52e11d86eb7a41f586f046c4110fea97b1
melaniequalls <melaniequalls@gmail.com>
2025-11-23 12:00:44 -0800

    Updated server.py

--- backend/server.py
+++ backend/server.py
@@ -9,9 +9,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # or "http://localhost:5173"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
